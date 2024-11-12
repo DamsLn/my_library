941dfdde6f559c0bdf406f294bd550f46eb2cf60
Damien Lasson <lasson.damienpro@gmail.com>
2024-11-12 21:17:35 +0100

    feat/admin: begins the development of the books management feature

---
+++ src/Controller/Admin/BookController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BookController extends AbstractController
+{
+    #[Route('/book', name: 'app_book')]
+    public function index(): Response
+    {
+        return $this->render('book/admin/index.html.twig', [
+            'controller_name' => 'BookController',
+        ]);
+    }
+}
