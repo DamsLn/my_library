042563c23298324007ed92e3e23406d215fdd877
Damien Lasson <lasson.damienpro@gmail.com>
2024-11-20 18:45:15 +0100

    feat/admin: adds contact form

---
+++ src/DTO/ContactDTO.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\DTO;
+
+use Symfony\Component\Validator\Constraints as Assert;
+use Webmozart\Assert\Assert as AssertAssert;
+
+class ContactDTO
+{
+    private const string NOT_BLANK_MESSAGE = 'Ce champ ne peut pas être vide';
+
+    #[Assert\Sequentially([
+        new Assert\NotBlank(
+            message: self::NOT_BLANK_MESSAGE
+        ),
+        new Assert\Email(
+            message: 'Vous devez saisir une adresse email valide.',
+        )
+    ])]
+    private string $email;
+
+    #[Assert\Sequentially([
+        new Assert\NotBlank(
+            message: self::NOT_BLANK_MESSAGE
+        ),
+        new Assert\Length(
+            max: 255,
+            maxMessage: 'L\'objet de votre demande doit contenir au maximum 255 caractères.'
+        )
+    ])]
+    private string $subject;
+
+    #[Assert\Sequentially([
+        new Assert\NotBlank(
+            message: self::NOT_BLANK_MESSAGE
+        ),
+        new Assert\Length(
+            min: 20,
+            max: 1000,
+            minMessage: 'Votre message doit contenir au moins 20 caractères.',
+            maxMessage: 'Votre message doit contenir au maximum 1000 caractères.'
+        )
+    ])]
+    private string $message;
+
+    /**
+     * @return string
+     */ 
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return self
+     */ 
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */ 
+    public function getSubject(): string
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param string $subject
+     * @return self
+     */ 
+    public function setSubject(string $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */ 
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     * @return self
+     */ 
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
---
+++ src/Form/ContactType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\DTO\ContactDTO;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'required' => true,
+            ])
+            ->add('subject', TextType::class, [
+                'label' => 'Objet de vote demande',
+                'required' => true,
+            ])
+            ->add('message', TextType::class, [
+                'label' => 'Votre message',
+                'required' => true,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ContactDTO::class,
+        ]);
+    }
+}
